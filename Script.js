91bddcbcf07d0da6099e35f1adbd4af7bbc92d55
Artem <artnovorai@gmail.com>
2016-12-11 15:45:06 +0200

    change the coordinates of the point

--- Script.js
+++ Script.js
@@ -3,4 +3,4 @@ var y1 = 123;
 var x2 = prompt( "Enter your number for x2", "32" );
 var y2 = prompt( "Enter your number for y2", "99")
 var result = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-alert ( "The distance between points A(13,23) and B(" + x2 + "," + y2 + ") is " + result);
+alert ( "The distance between points A(43,123) and B(" + x2 + "," + y2 + ") is " + result);
